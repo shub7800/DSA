2811b060d198c07804f289465162bd82bf98de20
Shubham Gupta <114250610+shub7800@users.noreply.github.com>
2024-02-21 16:53:12 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,24 @@
+public class ReverseNumber {
+    public static void getReverse(int num[]){
+        int first=0,last=num.length-1;
+        while(first<last){
+            //swap 
+            int temp=num[last];
+            num[last]=num[first];
+            num[first]=temp;
+            first++;
+            last--;
+
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+        getReverse(num);
+        //print 
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
